e8ce7e57d346507e6b588bb8c8313856753f5540
my name <myemailid@example.com>
2024-06-27 19:40:47 +0700

    sliver app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/components/my_current_location.dart';
+import 'package:food_delivery/components/my_desciption_box.dart';
 import 'package:food_delivery/components/my_drawer.dart';
+import 'package:food_delivery/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,11 +15,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-        backgroundColor: Theme.of(context).colorScheme.background,
+      drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+              title: const Text('title'),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Divider(
+                    indent: 25,
+                    endIndent: 25,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  // my current location
+                  const MyCurrentLocation(),
+                  // description box
+                  const MyDesciptionBox()
+
+                  //
+                ],
+              ))
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
       ),
-      drawer: MyDrawer(),
     );
   }
 }
